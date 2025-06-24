ede120d9bbe6ca9d2cbc14e9a245f678c4b7186a
saltywillams <saltyamogh19@gmail.com>
2025-06-24 18:56:22 -0400

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -690,8 +690,9 @@ async function startServer() {
     //
     // START SERVER
     //
-    app.listen(3000, () => {
-      console.log('🚀 Server running on http://localhost:3000');
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log('🚀 Server running on {PORT}');
     });
   } catch (err) {
     console.error('❌ Failed to start server:', err);
