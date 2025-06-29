0d283a56a0c1ba0e50d3bf6df99e07ba9b8708cf
saltywillams <saltyamogh19@gmail.com>
2025-06-29 18:00:36 -0400

    Reformatting Landing

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -7,37 +7,29 @@ import mySvg from './assets/discussion.svg';
 const Landing: React.FC = () => {
   const navigate = useNavigate();
 
-  
-  const handleAdminSignUp = () => {
-    navigate("/admin-signup");
-  };
-
-  const handleStudentSignUp = () => {
-    navigate("/student-signup");
-  };
-
-  const handleAdminLogIn = () => {
-    navigate("/login-admin");
-  };
-
-  const handleStudentLogIn = () => {
-    navigate("/login-student");
-  };
-
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="relative text-center">
-        <img src={mySvg} alt="My SVG" />
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
+        <img
+          src={mySvg}
+          alt="discussion illustration"
+          className="mb-[-60px] max-w-[123%] sm:mb-0 sm:max-w-full"
+        />
+        <div className="absolute inset-0 flex flex-col justify-center items-center">
           <h1 className="text-4xl montserrat-bold">EduForums</h1>
-          <div className="flex justify-center space-x-6 mt-4 mr-6">
-            <p className="text-lg cursor-pointer text-500" onClick={handleAdminSignUp}>Sign Up as Admin</p>
-            <p className="text-lg cursor-pointer text-500" onClick={handleStudentSignUp}>Sign Up as Student</p>
-          </div>
-          <div className="flex justify-center space-x-6 mb-2 mr-3">
-            <p className="text-lg cursor-pointer text-500" onClick={handleAdminLogIn}>Log In as Admin</p>
-            <p className="text-lg cursor-pointer text-500" onClick={handleStudentLogIn}>Log In as Student</p>
-          </div>
+         
+            <p className="text-lg cursor-pointer text-gray-500" onClick={() => navigate("/admin-signup")}>
+              Sign Up as Admin
+            </p>
+            <p className="text-lg cursor-pointer text-gray-500" onClick={() => navigate("/student-signup")}>
+              Sign Up as Student
+            </p>
+            <p className="text-lg cursor-pointer text-gray-500" onClick={() => navigate("/login-admin")}>
+              Log In as Admin
+            </p>
+            <p className="text-lg cursor-pointer text-gray-500" onClick={() => navigate("/login-student")}>
+              Log In as Student
+            </p>
         </div>
       </div>
     </div>
@@ -45,9 +37,3 @@ const Landing: React.FC = () => {
 };
 
 export default Landing;
-
-
-
-
-
-  
